8f8d156c1d5711bbaaed2c2c667d1989259a4177
moeen_yousafi <moeen.yousafi@gmail.com>
2022-07-29 09:18:34 +0500

    initial commit

---
+++ SnakeProgress/SnakeProgress/SnakeProgress.h
@@ -0,0 +1,18 @@
+//
+//  SnakeProgress.h
+//  SnakeProgress
+//
+//  Created by Moeen Arif on 28/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SnakeProgress.
+FOUNDATION_EXPORT double SnakeProgressVersionNumber;
+
+//! Project version string for SnakeProgress.
+FOUNDATION_EXPORT const unsigned char SnakeProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnakeProgress/PublicHeader.h>
+
+
